fdc7642a55f02ae48848c42c9a625b695c1af641
CeruleanDerpo <tramonti.luca18@gmail.com>
2024-06-15 16:39:06 +0200

    First commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include "blocks2stacks.h"
+
+#include <QApplication>
+#include <QLocale>
+#include <QTranslator>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QTranslator translator;
+    const QStringList uiLanguages = QLocale::system().uiLanguages();
+    for (const QString &locale : uiLanguages) {
+        const QString baseName = "blocks2stacks_" + QLocale(locale).name();
+        if (translator.load(":/i18n/" + baseName)) {
+            a.installTranslator(&translator);
+            break;
+        }
+    }
+    blocks2stacks w;
+    w.show();
+    return a.exec();
+}
